e05e76994af93c483f21ce773848f00f56340311
Olagunju Abdulrahman <abdulrahmanolagunju.1@gmail.com>
2020-11-10 11:29:21 +0100

    React Router  to configure and set up navigation

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -45,14 +45,14 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         if (props.dish != null) {
             return (
                 <div class="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.dish.comments} />
+                        </div>
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish.comments} />
-                    </div>
-                </div>
                 </div>
             );
         }
